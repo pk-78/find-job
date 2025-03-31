2d6c2ae18733bd29001d27249d0580a1ed32a2b0
Priyanshu Kushwaha <priyanshukushwaha311@gmail.com>
2025-04-01 01:02:02 +0530

    third commit

--- frontend/src/utils/constant.js
+++ frontend/src/utils/constant.js
@@ -1,8 +1,7 @@
-export const USER_API_END_POINT =
-  "https://find-job-z471.onrender.com/api/v1/user";
-export const JOB_API_END_POINT =
-  "https://find-job-z471.onrender.com/api/v1/job";
-export const APPLICATION_API_END_POINT =
-  "https://find-job-z471.onrender.com/api/v1/application";
-export const COMPANY_API_END_POINT =
-  "https://find-job-z471.onrender.com/api/v1/company";
+const render = "https://find-job-z471.onrender.com";
+const local = "http://localhost:8000";
+
+export const USER_API_END_POINT = `${local}/api/v1/user`;
+export const JOB_API_END_POINT = `${local}/api/v1/job`;
+export const APPLICATION_API_END_POINT = `${local}/api/v1/application`;
+export const COMPANY_API_END_POINT = `${local}/api/v1/company`;
