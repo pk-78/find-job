1088d9f5c6bc2384e935b0ded33e87e9cdf6e0cb
Priyanshu Kushwaha <priyanshukushwaha311@gmail.com>
2025-04-01 01:24:01 +0530

    third commit

--- backend/middlewares/isAuthenticated.js
+++ backend/middlewares/isAuthenticated.js
@@ -4,12 +4,12 @@ const isAuthenticated = async (req, res, next) => {
   try {
     const token = req.cookies.token;
     console.log(token);
-    // if (!token) {
-    //   return res.status(401).json({
-    //     message: "User not authenticated",
-    //     success: false,
-    //   });
-    // }
+    if (!token) {
+      return res.status(401).json({
+        message: "User not authenticated",
+        success: false,
+      });
+    }
     const decode = await jwt.verify(token, process.env.SECRET_KEY);
     if (!decode) {
       return res.status(401).json({
